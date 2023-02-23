b1920e45710d160ff4b969834b6ece3f2cf5c7a5
bhickta <nishantbhickta+code@gmail.com>
2023-02-23 15:00:05 +0530

    20 min

--- ssg.py
+++ ssg.py
@@ -4,6 +4,7 @@ from ssg.site import Site
 
 def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest}
-    Site = Site(**config)
-    Site.build()
-typer.run(main())
+    Site(**config).build()
+
+
+typer.run(main)
